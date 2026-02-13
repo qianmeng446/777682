457fd4fe4a11340714ee4bdc97af0ce250de14ab
qianmeng446 <qianmeng446@gmail.com>
2026-02-14 00:27:29 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -11,7 +11,7 @@ export default function PoweredBy(props) {
       <a
         href='https://777682.xyz'
         className='underline justify-start'>
-        //NotionNext {siteConfig('VERSION')}
+        千梦网络//NotionNext {siteConfig('VERSION')}
       </a>
       .
     </div>
