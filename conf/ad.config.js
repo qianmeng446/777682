1d63caee19607f07e0d9155ec989f5a331b34074
qianmeng446 <qianmeng446@gmail.com>
2026-03-04 00:50:25 +0800

    Update ad.config.js

--- conf/ad.config.js
+++ conf/ad.config.js
@@ -3,7 +3,8 @@
  */
 module.exports = {
   // 谷歌广告
-  ADSENSE_GOOGLE_ID: process.env.NEXT_PUBLIC_ADSENSE_GOOGLE_ID || '', // 谷歌广告ID e.g ca-pub-xxxxxxxxxxxxxxxx
+  ADSENSE_GOOGLE_ID: process.env.NEXT_PUBLIC_ADSENSE_GOOGLE_ID || 'ca-pub-1990364292853526', // 谷歌广告ID e.g ca-pub-xxxxxxxxxxxxxxxx<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=   ca-pub-1990364292853526"
+     crossorigin="anonymous"></script>
   ADSENSE_GOOGLE_TEST: process.env.NEXT_PUBLIC_ADSENSE_GOOGLE_TEST || false, // 谷歌广告ID测试模式，这种模式获取假的测试广告，用于开发 https://www.tangly1024.com/article/local-dev-google-adsense
   ADSENSE_GOOGLE_SLOT_IN_ARTICLE:
     process.env.NEXT_PUBLIC_ADSENSE_GOOGLE_SLOT_IN_ARTICLE || '3806269138', // Google AdScene>广告>按单元广告>新建文章内嵌广告 粘贴html代码中的data-ad-slot值
