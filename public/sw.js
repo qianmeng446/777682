00a0e1ef10cc4b83f20efe917485f46b946ae66d
qianmeng446 <qianmeng446@gmail.com>
2026-04-01 14:36:00 +0800

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10813727
+    "zoneId": 10814036
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
