3b95c8ebdb138efa9da3865c4bf609d2bfcd7afb
qianmeng446 <qianmeng446@gmail.com>
2026-05-02 00:52:17 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: '777682.XYZ',
+  HEO_HERO_TITLE_3: '777682.xyz',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: '6',
   HEO_HERO_TITLE_LINK: 'https://777682.xyz',
